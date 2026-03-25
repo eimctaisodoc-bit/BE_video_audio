77d16dad1465e79d59fd9ecae091f9a0bf796604
eimctaisodoc-bit <eimcta.isodoc@gmail.com>
2026-03-25 11:50:33 +0545

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const cors = require('cors')
 
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
